f953c62b848bed8c698c6e5ac117b64a63dbb0ee
lb <cmds12345678@163.com>
2020-04-02 01:37:26 +0800

    bug

--- schoolpark-pojo/src/main/java/cn/lovingliu/pojo/vo/PictureVO.java
+++ schoolpark-pojo/src/main/java/cn/lovingliu/pojo/vo/PictureVO.java
@@ -1,8 +1,14 @@
 package cn.lovingliu.pojo.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class PictureVO {
     private Integer pictureId;
     private String pictureName;
